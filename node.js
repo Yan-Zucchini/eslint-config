fb6861f5f1d11fac56c1522f39ef24f00acadd6b
Yanzuc <yanzucchini@gmail.com>
2023-12-29 08:50:14 -0300

    Teste de eslint 4

--- node.js
+++ node.js
@@ -22,6 +22,9 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "globals": {
+        "MyGlobal": true
+    },
     "rules": {
         "prettier/prettier": [
             "error",
